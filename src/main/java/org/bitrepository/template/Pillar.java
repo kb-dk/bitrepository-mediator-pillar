50f05c32f1b96d72ecc14af72fcd683678694bd8
bohlski <rbkr@kb.dk>
2021-11-29 14:42:38 +0100

    Init

---
+++ src/main/java/org/bitrepository/template/Pillar.java
@@ -0,0 +1,19 @@
+package org.bitrepository.template;
+
+import dk.kb.util.yaml.YAML;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Pillar {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+    private YAML config;
+
+    public Pillar(YAML config) {
+        log.debug("Running constructor HelloWorld()");
+        this.config = config;
+    }
+
+    public void start() {
+        System.out.println(config.getString("crypto.encryptionAlgo"));
+    }
+}
---
+++ src/main/java/org/bitrepository/template/PillarMain.java
@@ -0,0 +1,40 @@
+package org.bitrepository.template;
+
+import java.util.Arrays;
+import java.util.concurrent.Callable;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dk.kb.util.yaml.YAML;
+import picocli.CommandLine;
+
+@CommandLine.Command()
+public class PillarMain implements Callable<Integer>{
+        
+    private static final Logger log = LoggerFactory.getLogger(PillarMain.class);
+
+    @CommandLine.Parameters(index = "0", type = String.class, defaultValue = "src/main/conf/")
+    private String configPath;
+
+    @CommandLine.Parameters(index = "1", type = String.class, defaultValue = "src/main/conf/client-01.pem") // Default for now
+    private String keyfilePath;
+
+    @Override
+    public Integer call() throws Exception {
+        YAML config = new YAML(configPath + "intermediatorConfig.yaml"); // TODO probably move
+
+        Pillar pillar = new Pillar(config);
+        pillar.start();
+        return 0;
+    }
+    
+    
+    public static void main(String... args) {
+        System.out.println("Arguments passed by commandline is: " + Arrays.asList(args));
+        CommandLine app = new CommandLine(new PillarMain());
+        int exitCode = app.execute(args);                
+        System.exit(exitCode);
+    }            
+}
