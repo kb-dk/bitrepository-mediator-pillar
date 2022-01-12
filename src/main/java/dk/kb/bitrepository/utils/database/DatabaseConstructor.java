3f9fa72bb7b308c208440dfd1302c9b34324b1fc
Mathias Søby <masj@kb.dk>
2022-01-12 10:11:29 +0100

    It is now possible to parse SQL files to a String array. This allows for setting up the initial tables.

--- src/main/java/dk/kb/bitrepository/utils/database/DatabaseConstructor.java
+++ src/main/java/dk/kb/bitrepository/utils/database/DatabaseConstructor.java
@@ -3,25 +3,25 @@ package dk.kb.bitrepository.utils.database;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.swing.plaf.nimbus.State;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.sql.*;
 
-public class DatabaseManager {
-    private static final Logger log = LoggerFactory.getLogger(DatabaseManager.class);
+public class DatabaseConstructor {
+    private static final Logger log = LoggerFactory.getLogger(DatabaseConstructor.class);
     private static final String dbName = "testdb"; //TODO: Not hardcoded but dynamic
     private static final String username = "masj"; //TODO: Not hardcoded
     private static final String password = "masj"; //TODO: Not hardcoded
     private static final String databaseURL = "jdbc:postgresql://localhost:5432/" + dbName;
 
     public static void main(String[] args) throws IOException {
-        String filePath = "src/main/java/dk/kb/bitrepository/utils/database/create_tables.sql";
-        String[] query = new String[0];
+
+    }
+
+    private static String[] parseSQL(String filePath) {
+        String[] queries = new String[0];
 
         try {
             File file = new File(filePath);
@@ -34,18 +34,16 @@ public class DatabaseManager {
                 stringBuilder.append(currentLine);
             reader.close();
 
-            // Splitting the strong on ";" to separate requests.
-            query = stringBuilder.toString().split(";");
+            // Splitting the string on ";" to separate requests.
+            queries = stringBuilder.toString().split(";");
         } catch (IOException e) {
             log.error("Could not find file {}.", filePath, e);
             System.out.println("No file exists at the provided path.");
         }
-        String test = Files.readString(Path.of(filePath));
-        createTables(test);
-        //executeQuery(query);
+        return queries;
     }
 
-    public static void executeQuery(String[] query) {
+    public static void executeSQL(String[] query) {
         try (Connection connection = connect();
              Statement statement = connection.createStatement()) {
             System.out.println("Connection established to the database; trying to execute query.");
@@ -70,12 +68,8 @@ public class DatabaseManager {
     }
 
     public static void createTables(String query) {
-        try (Connection connection = connect();
-             Statement statement = connection.createStatement()) {
-            statement.executeQuery(query);
-        } catch (SQLException e) {
-            log.error("Something went wrong: ", e);
-            System.out.println("Something went wrong: " + e);
-        }
+        String filePath = "src/main/java/dk/kb/bitrepository/utils/database/create_tables.sql";
+        String[] queries = parseSQL(filePath);
+        executeSQL(queries);
     }
 }
