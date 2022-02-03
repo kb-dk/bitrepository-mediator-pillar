b20a6a70bdcb6907add806da1e61601b5d1753e1
Mathias Søby <masj@kb.dk>
2022-02-03 15:21:05 +0100

    Removed some constants that are not gonna be used.

--- src/main/java/dk/kb/bitrepository/mediator/utils/configurations/ConfigConstants.java
+++ src/main/java/dk/kb/bitrepository/mediator/utils/configurations/ConfigConstants.java
@@ -1,10 +1,5 @@
 package dk.kb.bitrepository.mediator.utils.configurations;
 
 public class ConfigConstants {
-    public static final String COMPONENT_ID = "pillarConfig.componentID";
-
-    public static final String PRIVATE_MESSAGE_DESTINATION = "pillarConfig.privateMessageDestination";
-
     public static final String CONFIGS_PATH = "src/main/resources/conf/mediatorConfigs.yml";
-
 }
